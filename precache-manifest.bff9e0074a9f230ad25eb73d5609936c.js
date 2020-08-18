973a58b632ba6b9503d610811ebffe52f2284d8d
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-08-18 04:31:23 +0200

    Updates

---
+++ precache-manifest.bff9e0074a9f230ad25eb73d5609936c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8060518273aaf166f7ac445da4531ee",
+    "url": "/Movies-Quiz-App/index.html"
+  },
+  {
+    "revision": "6ebf8cea0c06cae8f99b",
+    "url": "/Movies-Quiz-App/static/js/2.1632e77e.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/Movies-Quiz-App/static/js/2.1632e77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0fba2e7a46c7969216",
+    "url": "/Movies-Quiz-App/static/js/main.e5978153.chunk.js"
+  },
+  {
+    "revision": "bf31c380f5920d7fdf45",
+    "url": "/Movies-Quiz-App/static/js/runtime-main.c11c4ddb.js"
+  },
+  {
+    "revision": "ff9cde7a0ab2bf4581c7b3689a794f7c",
+    "url": "/Movies-Quiz-App/static/media/BG.ff9cde7a.jpg"
+  }
+]);
